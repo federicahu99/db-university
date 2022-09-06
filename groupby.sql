e65e131bde8f86b10a82aae9340f3761d686b938
feddericahu99 <federica.hu339@gmail.com>
2022-09-06 16:31:16 +0200

    es. 4

--- groupby.sql
+++ groupby.sql
@@ -12,8 +12,12 @@ GROUP BY `office_number`;
 
 --3. Calcolare la media dei voti di ogni appello d'esame
 
-
+SELECT AVG(`vote`) FROM `exam_student` 
+WHERE `vote` > 17 
+GROUP BY `exam_id`;
 
 --4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
-
+SELECT COUNT(`name`) AS `departments_degrees`
+FROM `degrees`
+GROUP BY `department_id`;
