6eb34ba61dd3b14da7be0044656812f4be59d1b8
feddericahu99 <federica.hu339@gmail.com>
2022-09-07 19:51:24 +0200

    es 4

--- joing.sql
+++ joing.sql
@@ -27,7 +27,14 @@ WHERE `teachers`.`id` = 44;
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
 
-
+SELECT `students`.`id`, `courses`.`name`, `departments`.`name` 
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`
+AND `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
@@ -38,6 +45,7 @@ WHERE `teachers`.`id` = 44;
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
 
 
 
