409e4ebb77678772a8e6b585ce5a2ef886068e7b
feddericahu99 <federica.hu339@gmail.com>
2022-09-07 16:56:38 +0200

    es. 3

--- joing.sql
+++ joing.sql
@@ -17,7 +17,7 @@ AND `departments`.`name` = "Dipartimento di Neuroscienze"
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
-SELECT `courses`.`id`, `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+SELECT `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
 FROM `courses`
 JOIN `course_teacher`
 ON `course_teacher`.`course_id` = `courses`.`id`
@@ -28,25 +28,11 @@ WHERE `teachers`.`id` = 44;
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
 
- SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name`
-FROM `students`
-JOIN `degrees`
-ON `students`.`degree_id` = `degrees`.`id`
-JOIN `departments`
-ON `degrees`.`department_id` = `departments`.`id`  
-ORDER BY `students`.`name`, `students`.`surname`  ASC;
+
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
-SELECT `degrees`.`name` AS `name_degree`, `courses`.`name` AS `name_course`, `teachers`.`name`, `teachers`.`surname`
-FROM `degrees`
-JOIN `courses`
-ON `courses`.`degree_id` = `degrees`.`id`
-JOIN `course_teacher` 
-ON `course_teacher`.`course_id` = `courses`.`id`
-JOIN `teachers`
-ON `course_teacher`.`teacher_id` = `teachers`.`id`
-ORDER BY `degrees`.`name` ASC;
+
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
