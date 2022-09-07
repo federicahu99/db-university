7b6a243a297370b8baff9f11f7c9fe8a929f8b3e
feddericahu99 <federica.hu339@gmail.com>
2022-09-07 14:06:32 +0200

    fixed es 3

--- database.sql
+++ database.sql
@@ -15,7 +15,7 @@ WHERE `cfu` > 10;
 
 SELECT * 
 FROM `students` 
-WHERE YEAR(`date_of_birth`) < 1993;
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURDATE()) > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
