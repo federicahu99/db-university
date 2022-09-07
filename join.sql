2e28b08b4b2924589c090ee256e97ef5caa994cf
Federica Hu <103883068+federicahu99@users.noreply.github.com>
2022-09-07 18:57:22 +0200

    Update and rename joing.sql to join.sql

--- join.sql
+++ join.sql
@@ -8,22 +8,11 @@ WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
-SELECT `degrees`.`name`, `degrees`.`level`, `departments`.`id`, `departments`.`name`
-FROM `degrees`
-JOIN `departments`
-ON `degrees`.`department_id` = `departments`.`id`
-WHERE `degrees`.`level` = "Magistrale"
-AND `departments`.`name` = "Dipartimento di Neuroscienze"
+
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
-SELECT `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
-FROM `courses`
-JOIN `course_teacher`
-ON `course_teacher`.`course_id` = `courses`.`id`
-JOIN `teachers`
-ON `course_teacher`.`teacher_id` = `teachers`.`id`
-WHERE `teachers`.`id` = 44;
+
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
