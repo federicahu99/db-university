85f34a929073af7f3e4f288af5a001993889524e
feddericahu99 <federica.hu339@gmail.com>
2022-09-08 19:54:35 +0200

    es 6

--- joing.sql
+++ joing.sql
@@ -38,11 +38,27 @@ AND `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
-
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name` 
+FROM `students` 
+JOIN `degrees` 
+ON `degrees`.`id` = `students`.`degree_id` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`.`department_id` 
+ORDER BY `students`.`surname` ASC, `students`.`name` ASC;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
-
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` 
+FROM `teachers` 
+JOIN `course_teacher` 
+ON `teachers`.`id` = `course_teacher`.`teacher_id` 
+JOIN `courses` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `degrees` 
+ON `degrees`.`id` = `courses`.`degree_id` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = "dipartimento di matematica";
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 -- superare ciascuno dei suoi esami
